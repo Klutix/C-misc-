b243df8b160cf16b4cc54b8464bdbd4c5386c9f0
Gabriel Wohlford <GabrielWohlford@gmail.com>
2019-10-02 02:50:09 -0500

    added yellow folder labels

--- misc.cpp
+++ misc.cpp
@@ -1,14 +1,20 @@
 //Klutix
-//a recursive function that allows you to select a file from a path. 
-//  (If no path is provided the working directory is used)
-std::string FileDialogPrompt(std::string path = "") {
-	int choice, i = 1;
+//allows you to select a file from a path. If no path is provided the working directory is used.
+std::string FileDialogPrompt(std::string path = "",bool cls = false) {
 	std::vector<std::string> directory;
+	int choice, i = 1;
+	if (cls)	
+		system("CLS");
 	if (path == "")
 		path = std::experimental::filesystem::current_path().u8string();
-	std::cout << "\nShowing path.." << path << std::endl << std::endl;
+	std::cout << "\nContents of: " << path << std::endl << std::endl;
 	for (const auto & entry : fs::directory_iterator(path)) {
-		std::cout << "\t" << i << ") " << entry.path() << std::endl;
+		if (!fs::is_directory(entry.path()))
+			std::cout << "\t" << i << ") " << entry.path().filename() << std::endl;
+		else {
+			printf("\t%d) \x1B[93mFOLDER \033[0m", i); //this magic formula makes yellow..
+			std::cout << entry.path().filename().u8string() << std::endl;
+		}
 		directory.push_back(entry.path().u8string());
 		i++;
 	}
@@ -18,10 +24,10 @@ std::string FileDialogPrompt(std::string path = "") {
 	choice -= 1; //subract 1 because selection starts at 1
         if (choice + 1 == 0) {
 		std::filesystem::path pPath = std::filesystem::path(path);
-		FileDialogPrompt(pPath.parent_path().u8string());
+		FileDialogPrompt(pPath.parent_path().u8string(), cls);
 	}
 	else if (fs::is_directory(directory[choice]))
-		FileDialogPrompt(directory[choice]);
+		FileDialogPrompt(directory[choice], cls);
 	else
 		return directory[choice];
 }
